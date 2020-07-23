c760d5b26951bdb4561efa4a4a28663840e542bb
Tom Checkley <tomc@element78.co.uk>
2020-07-23 09:38:26 +0100

    nav scss mixns & variables

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { RecipesComponent } from './components/recipes/recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    HomeComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
