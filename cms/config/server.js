b41df3b82dd60bf8507a0b479fbe619bb298e38f
Tom Checkley <tomc@element78.co.uk>
2020-07-23 06:44:53 +0100

    Strapi CMS init

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f397736b0c759c5c341173a4890511d9'),
+    },
+  },
+});
